9a787717e8e1d2b99f3f412ab89dd343709c393c
Cesare <runozo@gmail.com>
2014-12-25 22:53:16 +0100

    Deleted logs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git archive master | bzip2 >/tmp/source-tree.tar.bz2
 scp /tmp/source-tree.tar.bz2 rune@bigdog:/tmp
-ssh rune@bigdog 'mkdir /tmp/ttt;cd /tmp/ttt;tar xvjf ../source-tree.tar.bz2;cp -rf /tmp/ttt/* /var/www/beatmatic.it/'
-ssh rune@bigdog 'rm -rf /tmp/ttt;rm /tmp/source-tree.tar.bz2'
+ssh rune@bigdog 'mkdir /tmp/ttt;cd /tmp/ttt;tar xvjf ../source-tree.tar.bz2'
+ssh rune@bigdog 'cp -rf /tmp/ttt/* /var/www/beatmatic.it/'
+# ssh rune@bigdog 'rm -rf /tmp/ttt;rm /tmp/source-tree.tar.bz2'
