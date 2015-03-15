5af2a79285dc6ac42ee70b16cc0ff436587e3f5c
Cesare <runozo@gmail.com>
2015-03-15 18:17:36 +0100

    Make image responsive

--- beatmatic/frontend.py
+++ beatmatic/frontend.py
@@ -2,6 +2,7 @@
 
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+from datetime import date
 
 home = Blueprint('home', __name__,
                  template_folder='templates')
@@ -10,6 +11,6 @@ home = Blueprint('home', __name__,
 @home.route('/')
 def show():
     try:
-        return render_template('base.html')
+        return render_template('base.html', year=date.today().year)
     except TemplateNotFound:
         abort(404)
